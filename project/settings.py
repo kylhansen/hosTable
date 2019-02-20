9b5e9fd2934a795589e492b3e8620e191daafd78
kylhansen <kylhansen@westmont.edu>
2019-02-20 07:39:31 -0800

    stackexchange attempt

--- project/settings.py
+++ project/settings.py
@@ -131,7 +131,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATICFILES_STORAGE = ‘whitenoise.storage.CompressedManifestStaticFilesStorage’
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 #COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
