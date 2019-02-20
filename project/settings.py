bdaa79ce60f732223ded2cba07078d010df1a305
kylhansen <kylhansen@westmont.edu>
2019-02-19 19:48:12 -0800

    settings

--- project/settings.py
+++ project/settings.py
@@ -14,9 +14,6 @@ import os
 from django.urls import reverse
 import django_heroku
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -28,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '04w34kraea!2v^4#6h7w7lc(9d3y_7th*f+x0yve_5li$1ob-5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -170,3 +167,6 @@ except ImportError:
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse('user-view', kwargs={'pk': u.pk})
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
