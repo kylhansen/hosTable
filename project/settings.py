a453f737aba251732f5c9417b9b91ff9a1b52490
kylhansen <kylhansen@westmont.edu>
2019-02-19 21:11:29 -0800

    trying to get it to run still...

--- project/settings.py
+++ project/settings.py
@@ -167,19 +167,13 @@ USE_TZ = True
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
