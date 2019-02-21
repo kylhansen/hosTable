885ce5d065b2868f1d3b443f5d4112bf6ae6e715
kylhansen <kylhansen@westmont.edu>
2019-02-20 19:34:09 -0800

    fixed email sending

--- project/settings.py
+++ project/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '04w34kraea!2v^4#6h7w7lc(9d3y_7th*f+x0yve_5li$1ob-5'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['*']
 
@@ -141,14 +141,14 @@ LOGOUT_REDIRECT_URL = 'home'
 
 
 # Email setup
-
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = os.environ.get('HOSTABLE_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('HOSTABLE_PASS')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 # Absolute URL Override
 ABSOLUTE_URL_OVERRIDES = {
